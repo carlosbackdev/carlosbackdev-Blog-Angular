9d854b7d2f9c1ebf475b7189c2f8447c31ae9b4b
carlos <carloslospinos@gmail.com>
2025-03-13 02:22:42 +0100

    tecnologias iniciado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { TecnologyComponent } from "./components/tecnology/tecnology.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, InicioComponent],
+  imports: [RouterOutlet, NavComponent, InicioComponent, TecnologyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/tecnology/tecnology.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnology',
+  imports: [],
+  templateUrl: './tecnology.component.html',
+  styleUrl: './tecnology.component.scss'
+})
+export class TecnologyComponent {
+
+}
